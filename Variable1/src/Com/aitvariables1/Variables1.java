b82f97ff015cbbd599bc16d5c885ad3f053afbee
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-06-04 12:55:40 +0530

    Variables1

---
+++ Variable1/src/Com/aitvariables1/Variables1.java
@@ -0,0 +1,17 @@
+package Com.aitvariables1;
+
+public class Variables1 
+{
+  static int c=30; //static variable
+  static int a=10;  //instance variable
+	public static void main(String[] args)
+	{
+		int b=20; // local variable
+		System.out.println(c);
+		System.out.println(b);
+	    System.out.println(a);
+		
+
+	}
+
+}
