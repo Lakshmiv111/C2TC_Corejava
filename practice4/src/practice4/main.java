f1d1bca12d37b56510530916c23413060bd23657
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-06-04 11:12:40 +0530

    practice4

---
+++ practice4/src/practice4/main.java
@@ -0,0 +1,12 @@
+package practice4;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true)
+			System.out.println("Technoserve");
+			
+		}
+
+	}
