b54d840bdc20d94615bad0e3559bf55cfa9ba102
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-06-04 11:23:01 +0530

    BreakWhileLoop

---
+++ practice10/src/practice10/BreakWhileLoop.java
@@ -0,0 +1,17 @@
+package practice10;
+
+public class BreakWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		while(a<=0) {
+			if(a==5)
+		break;
+		System.out.print("\n\tStatement:" + a);
+		a++;
+		}
+ System.out.println("\n\tEnd of program.");
+	}
+
+}
