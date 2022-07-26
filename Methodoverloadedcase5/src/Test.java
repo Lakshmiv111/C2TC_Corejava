f42a3ce07197fac1698d389234cd1e82d07a2408
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-07-26 15:09:53 +0530

    methodoverloadedcase5

---
+++ Methodoverloadedcase5/src/Test.java
@@ -0,0 +1,20 @@
+
+ class Test
+ {
+	 public void m1(int i,float f)
+	 {
+		 System.out.println("int-float version");
+	 }
+public void m1(float f, int i)
+{
+	System.out.print("float-int version");
+}
+	public static void main(String[] args)
+	{
+		Test t = new Test();
+		t.m1(10,10.5f);
+		t.m1(10.5f,10);
+
+	}
+
+}
