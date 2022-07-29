502274325f2c3d51534c9ed4d7f837e7474696f8
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-07-29 15:27:53 +0530

    Interface3

---
+++ Interface3/src/Bike.java
@@ -0,0 +1,33 @@
+interface Bike
+
+{
+ void getMileage();
+
+}
+
+class R15 implements Bike
+
+{
+
+public void getMileage()
+
+{
+
+System.out.print("Mileage is 40 Km/L");
+
+}
+
+}
+
+ class InterfaceExample
+ {
+
+public static void main(String args[])
+
+{
+	R15 bike = new R15();
+
+	bike.getMileage();
+}
+
+}
