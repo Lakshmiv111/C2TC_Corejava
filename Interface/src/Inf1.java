097bb6e4e4d4a5677484b6b0327e23594e9f0081
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-07-29 15:09:14 +0530

    Interface

---
+++ Interface/src/Inf1.java
@@ -0,0 +1,29 @@
+interface Inf1
+
+{
+
+public void method1();
+
+}
+
+ class Demo implements Inf1
+{
+
+public void method1()
+
+{
+
+System.out.println("method1");
+
+}
+
+public static void main(String args[])
+
+{
+
+Demo obj = new Demo();
+
+obj.method1();
+
+}
+}
