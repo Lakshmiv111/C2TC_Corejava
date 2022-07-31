6502f7617b1f0f4b3de9307828ac6da1cac5f3df
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-07-31 13:19:29 +0530

    Lambda Expression topic

---
+++ Lambda Expression/src/Sayable.java
@@ -0,0 +1,21 @@
+class Sayable {
+
+public void say(){
+
+System.out.println("I have nothing to say");
+
+}
+
+public class Main{
+
+public static void main(String[] args)
+
+{
+
+Sayable s= new Sayable();
+
+s.say();
+
+}
+}
+}
