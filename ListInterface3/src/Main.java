6d10fba0fa5a9c884eb3b648d0313c85aa0ac902
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-08-02 21:26:07 +0530

    addAll() method

---
+++ ListInterface3/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+
+{
+
+public static void main(String [] args)
+
+{
+
+List<String> val1 = new ArrayList<>();
+
+val1.add("corona can be stopped");
+
+List<String> val2 = new ArrayList<>();
+
+val2.add("stay at home");
+
+val1.addAll(val2);
+
+System.out.println(val1);
+
+}
+
+}
