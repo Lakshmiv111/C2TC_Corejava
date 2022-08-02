a91b070c5e194659d9a49233ecae970ac6af662a
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-08-02 21:49:30 +0530

    contains() method

---
+++ ListInterface6/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+
+{
+
+public static void main(String [] args)
+
+{
+
+List<String> var = new ArrayList<String>();
+
+var.add("pen");
+
+var.add("pencil");
+
+var.add("eraser");
+
+System.out.println("List " +var);
+
+boolean res= var.contains("pencil");
+
+System.out.println("Is pencil present in the list: " + res);
+
+}
+
+}
