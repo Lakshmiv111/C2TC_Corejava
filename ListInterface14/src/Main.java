290562882dc592a27abea6b39dbd34bb2e8ea8e6
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-08-03 13:33:40 +0530

    fill() method

---
+++ ListInterface14/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main
+
+{
+
+public static void main(String[] args)
+
+{
+
+List<String> arrlist = new ArrayList<>();
+
+arrlist.add("java");
+
+arrlist.add("Technical");
+
+arrlist.add("webinar");
+
+System.out.println("List elements before Replacements: "+arrlist);
+
+Collections.fill(arrlist,"technology");
+
+System.out.println("List elements after Replacements: "+arrlist);
+
+}
+
+}
