6f586a84b803b175a6c0154acbf543275ee76321
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-08-03 14:21:38 +0530

    Program/code

---
+++ ListInterface19/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main
+
+{
+
+public static void main(String args[])
+
+{
+
+LinkedList<Integer> list=new
+LinkedList<Integer>();
+
+Scanner s=new Scanner(System.in);
+
+int num=s.nextInt();
+
+while(num!=0)
+
+{
+
+list.add(s.nextInt());
+
+num--;
+
+}
+
+Collections.shuffle(list);
+
+System.out.println(list);
+
+}
+
+}
